e4410028a41830bf31bd56844431070984b912a7
saisathwik03 <saisathwik173@gmail.com>
2024-03-22 17:22:10 +0530

    for checking

--- ranker.py
+++ ranker.py
@@ -4,5 +4,5 @@ class Ranker:
         for url in results:
             text = index[url]
             score = text.lower().count(keyword.lower())
-            ranked_results[url] = score
+            ranked_results[url] =         score
         return dict(sorted(ranked_results.items(), key=lambda x: x[1], reverse=True))
